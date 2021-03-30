693cc73cdfb9113013af1d829c4a5e8a3791fc49
Halbard <HalbardHobby@users.noreply.github.com>
2021-03-30 12:40:41 -0500

    🛡 Added model validations

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,11 @@ class Log < ApplicationRecord
   belongs_to :user
   scope :intakes, -> {where(intake: true)}
   scope :expenses, -> {where(intake: false)}
+  
+  validates :description, presence: true
+  validates :calories, numericality: { greater_than_or_equal_to: 0 }
+  validates :protein, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :carb, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :fat, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
+  validates :alcochol, numericality: { greater_than_or_equal_to: 0, allow_nil: true }
 end
