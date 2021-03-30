5d7722f7341843393e79eafc657d5071b65704d3
Halbard <HalbardHobby@users.noreply.github.com>
2021-03-29 20:32:00 -0500

    🎮 Added intake controller

---
+++ app/controllers/intakes_controller.rb
@@ -0,0 +1,28 @@
+class IntakesController < ApplicationController
+
+    def new
+      @log = Log.new(intake: true)
+    end
+  
+    def index
+      @logs = current_user.logs.intakes
+    end
+  
+    def create
+      @log = Log.new(
+        log_params.merge(user_id: current_user.id, intake: true)
+      )
+  
+      if @log.save
+        redirect_to logs_url
+      else
+        render :new
+      end
+    end
+  
+    private
+      def log_params
+        params.require(:log).permit(:calories, :intake, :date, :description, :protein, :carb, :fat, :alcochol)
+      end
+  end
+  
