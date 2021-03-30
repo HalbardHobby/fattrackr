fbf426a914fde606f9f1a75ea7ff451555a68f71
Halbard <HalbardHobby@users.noreply.github.com>
2021-03-30 14:59:31 -0500

    🏗 scaffolded dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'dashboard#index'
+  get '/dashboard/shared/:token', to: 'dashboard#shared'
 
   resources :logs
   resources :expenses, only: [:new, :create, :index]
   resources :intakes, only: [:new, :create, :index]
 
   devise_for :users
-  root 'logs#index'
+  root 'dashboard#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
