5ee5563d88ee2803f886195c02992a65529e88c3
Halbard <HalbardHobby@users.noreply.github.com>
2021-03-29 20:18:12 -0500

    🏗 built new controller

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,27 @@
+class ExpensesController < ApplicationController
+
+  def new
+    @log = Log.new(intake: false)
+  end
+
+  def index
+    @logs = current_user.logs.intakes
+  end
+
+  def create
+    @log = Log.new(
+      log_params.merge(user_id: current_user.id, intake: false)
+    )
+
+    if @log.save
+      redirect_to logs_url
+    else
+      render :new
+    end
+  end
+
+  private
+    def log_params
+      params.require(:log).permit(:calories, :date, :description)
+    end
+end
