a3a2aa216d32ce51ec89a09cce8e5d2a4ee22329
Halbard <HalbardHobby@users.noreply.github.com>
2021-03-30 20:29:08 -0500

    🛠 Added basic token functionality

--- app/controllers/token_controller.rb
+++ app/controllers/token_controller.rb
@@ -1,12 +1,27 @@
+require 'securerandom'
+
 class TokensController < ApplicationController
+  before_action :set_token, only: [:destroy]
 
   def index
+    @tokens = current_user.tokens
   end
 
   def create
+    uuid = SecureRandom.uuid
+    @token = Token.new(user_id: current_user.id, token: uuid)
+    
+    if @token.save
+      redirect_to tokens_url
   end
 
   def destroy
+    @token.destroy
+    redirect_to tokens_url
   end
 
+  private
+    def set_token
+      @token = Token.find(params[:id])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_29_173344) do
+ActiveRecord::Schema.define(version: 2021_03_30_200336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2021_03_29_173344) do
     t.float "carb"
     t.float "fat"
     t.float "alcochol"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tokens", force: :cascade do |t|
+    t.string "token"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
